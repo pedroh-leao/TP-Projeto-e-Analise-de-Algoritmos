fd81afe7d32c7388ffa230c9741fa60218e522d0
matheusprv <matheusprv2003@gmail.com>
2024-09-20 10:42:44 -0300

    Correção de comentários e remoção de variável global

--- SAT/sat.py
+++ SAT/sat.py
@@ -1,6 +1,5 @@
 import time
 
-
 def le_problema(arquivo_entrada: str):
     with open(arquivo_entrada, 'r') as arquivo:
         qtd_variaveis = int(arquivo.readline().strip())
@@ -12,59 +11,58 @@ def le_problema(arquivo_entrada: str):
 
     return qtd_variaveis, entrada
 
+
 def solucao_inicial(qtd_variaveis: int):
     return [None] * qtd_variaveis
 
+
 def verificar_solucao(problema, solucao):
-    # Verificando se é completa
+    # Verificando se e completa
     for variavel in solucao:
         if variavel is None:
             return False
 
     for clausula in problema:
         satisfeita = False
-        # Toda clausula terá sempre três literais
         for variavel, valor_atual in zip(clausula, solucao):
             if variavel == 1:
                 satisfeita = satisfeita or valor_atual
             elif variavel == 0:
                 satisfeita = satisfeita or not valor_atual
 
-        # Se uma clausula não foi satisfeita, não é necessário seguir
-        # as disjunções se tornarão falsas
+        # Se uma clausula nao foi satisfeita, nao e necessario seguir
+        # as disjuncoes se tornarao falsas
         if not satisfeita:
             return False
     return True
 
 
-# A cláusula é computável quando todos os literais após o i sõ valores diferentes de -1
-# pois até o i-ésimo eles já foram preenchidos
-def clausula_computavel(i, clausula):
-    tamanho_clausula = len(clausula)
-    
-    for j in range(i, tamanho_clausula):
+# A clausula e computavel quando todos os literais apos o i sao valores diferentes de -1
+# pois ate o i-esimo eles ja foram preenchidos
+def clausula_computavel(i, clausula, qtd_variaveis):  
+    for j in range(i, qtd_variaveis):
         if clausula[j-1] == -1:
             return False
     
 
 def construir_candidatos(solucao, i, problema, dominio):
-    # Não é possível fazer mais testes, pois s já possui todos os valores preenchidos
+    # Nao e possivel fazer mais testes, pois solucao ja possui todos os valores preenchidos
     if i > len(solucao): return []
        
     candidatos = dominio.copy()
     s_temp = solucao.copy()
-    
-    # Testando cada valor do domínio
+    qtd_variaveis = len(problema[0])
+
+    # Testando cada valor do dominio
     for c in dominio:
         s_temp[i-1] = c
 
-        # Verifica se todas as clausulas possíveis podem ser satisfeitas com o valor candidato atual
-        # Se uma cláusula for falsa, já cancela o candidato atual        
+        # Verifica se todas as clausulas possiveis podem ser satisfeitas com o valor candidato atual
         for clausula in problema:
             satisfeita = False  
 
-            # Verificamos se os itens após o i-ésimo não são -1, porque, se forem, nao podemos verificar a clausula ainda
-            if clausula_computavel(i, clausula):
+            # Verificamos se os itens após o i-esimo nao sao -1, porque, se forem, nao podemos verificar a satisfabilidade da clausula ainda
+            if clausula_computavel(i, clausula, qtd_variaveis):
                 for variavel_atual, literal in enumerate(clausula):
                     if literal == 1:
                         satisfeita = satisfeita or s_temp[variavel_atual]
@@ -73,41 +71,39 @@ def construir_candidatos(solucao, i, problema, dominio):
             else:
                 continue
 
-            # Se não satisfez a cláusula atual, não poderá satisfazer as próximas, então o ramo deste candidato pode ser removido
+            # Se nao satisfez a clausula atual, nao podera satisfazer as proximas, entao o ramo deste candidato pode ser removido
             if not satisfeita:
                 candidatos.pop(0)
                 break
     return candidatos
 
 
-global finished
 def backtrack(solucao, i, problema):
     dominio = [False, True]
-    global finished
 
-    #print("Solucao parcial:", solucao)
     if verificar_solucao(problema, solucao):
-        finished = True
+        return True
     else:
         i = i + 1
         candidatos = construir_candidatos(solucao, i, problema, dominio)
         for c in candidatos:
             solucao[i-1] = c
-            backtrack(solucao, i, problema)
+            finished = backtrack(solucao, i, problema)
             if finished:
-                return 
+                return True
             solucao[i-1] = None
+
+    return False
 
 
 def SAT(caminho_problema):
-    global finished
-    finished = False
     inicio = time.time()
     qtd_variaveis, problema = le_problema(caminho_problema)
     solucao = solucao_inicial(qtd_variaveis)
-    backtrack(solucao, 0, problema)
+    solucao_encontrada = backtrack(solucao, 0, problema)
     fim = time.time()
-    if finished:
+
+    if solucao_encontrada:
         print("Solução encontrada:", solucao)
     else:
         print("Solução não encontrada")
@@ -119,5 +115,5 @@ def SAT(caminho_problema):
 if __name__ == "__main__":
     for i in range(10):
         print(f"Problema {i+1}")
-        SAT(f"SAT/entrada{i+1}.txt")
+        SAT(f"./entrada{i+1}.txt")
         print("\n")
